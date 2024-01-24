c3cd6d467f2a17d17a708ad56e9b24138f6730ff
Mauricio Ortega <maudan1098@gmail.com>
2024-01-23 21:33:25 -0600

    Pin assignment was corrected

--- include/LedBarConfig.h
+++ include/LedBarConfig.h
@@ -3,11 +3,11 @@
 #ifndef _LedBar_Config_h
 #define _LedBar_Config_h
 
-#define SEG_1_PIN 4
-#define SEG_2_PIN 5
-#define SEG_3_PIN 6
-#define SEG_4_PIN 7
-#define SEG_5_PIN 8
-#define SEG_6_PIN 9
+#define SEG_1_PIN 19
+#define SEG_2_PIN 18
+#define SEG_3_PIN 5
+#define SEG_4_PIN 4
+#define SEG_5_PIN 2
+#define SEG_6_PIN 16
 
 #endif
